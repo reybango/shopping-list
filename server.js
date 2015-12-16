1ab79bc53287b0b0a4a0a8538dac888d1d3bb892
Rey Bango <reybango@gmail.com>
2015-12-16 18:02:09 -0500

    Update port listening code due to Heroku need

--- server.js
+++ server.js
@@ -92,5 +92,7 @@ app.put('/items/:id', jsonParser, function(req,res){
 });
 
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
