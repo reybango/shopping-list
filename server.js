e71c69e285b6a8eeb8d4ce9f5a01426bd884c5dd
Rey Bango <reybango@gmail.com>
2015-12-15 22:22:17 -0500

    Wired up the Storage object and Express stuff

--- server.js
+++ server.js
@@ -5,3 +5,28 @@ var Storage = function() {
     this.id = 0;
 }
 
+Storage.prototype.add = function(name) {
+    
+    var item = {name: name, id: this.id};
+    this.items.push(item);
+    this.id += 1;
+    return item;
+
+}
+
+var storage = new Storage();
+storage.add('Broad beans');
+storage.add('Tomatoes');
+storage.add('Peppers');
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/items', function(req, res){
+   
+   res.json(storage.items); 
+    
+});
+
+app.listen(8080);
+
