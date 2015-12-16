753e0e6afcbcc31e554f9dca9978bafe2b942140
Rey Bango <reybango@gmail.com>
2015-12-16 16:01:23 -0500

    Added POST & DELETE routes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 var Storage = function() {
     this.items = [];
@@ -10,6 +12,16 @@ Storage.prototype.add = function(name) {
     var item = {name: name, id: this.id};
     this.items.push(item);
     this.id += 1;
+    return item;
+
+}
+
+Storage.prototype.delete = function(id) {
+    
+    var item = this.items[id].name, i = 0;
+    
+    i = id - 1;
+    this.items.splice(i,1);
     return item;
 
 }
@@ -26,6 +38,23 @@ app.get('/items', function(req, res){
    
    res.json(storage.items); 
     
+});
+
+app.post('/items', jsonParser, function(req,res){
+   if(!req.body) {
+       return res.sendStatus(400)
+   }
+   
+   var item = storage.add(req.body.name);
+   res.status(201).json(item); 
+});
+
+app.delete('/items/:id', function(req,res){
+   
+   var id = req.params.id, item = '';
+   item = storage.delete(id);
+   res.status(200).send(item); 
+
 });
 
 app.listen(8080);
